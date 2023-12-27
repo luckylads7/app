d16a1582f589d7fabf44b442f216ed10a0645960
luckylads7 <104345281+luckylads7@users.noreply.github.com>
2023-12-27 14:06:19 +0530

    commitment 2

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt=require("mongoose-encryption");
-
+const md5=require("md5");
 const app = express();
 const port = 3000;
 
@@ -23,7 +22,7 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 const User= new mongoose.model("User",userSchema);
 app.get("/", function (req, res) {
     res.render("home.ejs");
@@ -40,7 +39,7 @@ app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
     
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save()
         .then(()=>{
@@ -53,7 +52,7 @@ app.post("/register",function(req,res){
 });
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username })
         .then(foundUser => {
